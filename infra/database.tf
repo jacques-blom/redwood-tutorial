23c3c36137203b798be06f5b8770a46ca6dcef96
Jacques Blom <39128232+jacquesnf@users.noreply.github.com>
2020-10-18 18:47:31 +0200

    SSM

--- infra/database.tf
+++ infra/database.tf
@@ -10,24 +10,24 @@ data "aws_subnet_ids" "all" {
   vpc_id = data.aws_vpc.default.id
 }
 
-# data "aws_kms_secrets" "database_password" {
-#   secret {
-#     name    = "value"
-#     payload = ""
+data "aws_kms_secrets" "database_password" {
+  secret {
+    name    = "value"
+    payload = ""
 
-#     context {
-#       service = local.name
-#     }
-#   }
-# }
+    context {
+      service = local.name
+    }
+  }
+}
 
-# resource "aws_ssm_parameter" "database_password" {
-#   name      = "/${local.name}/database/password"
-#   type      = "SecureString"
-#   value     = "${data.aws_kms_secrets.database_password.plaintext["value"]}"
-#   key_id    = "${aws_kms_key.main.arn}"
-#   overwrite = "true"
-# }
+resource "aws_ssm_parameter" "database_password" {
+  name      = "/${local.name}/database/password"
+  type      = "SecureString"
+  value     = "${data.aws_kms_secrets.database_password.plaintext["value"]}"
+  key_id    = "${aws_kms_key.ssm.arn}"
+  overwrite = "true"
+}
 
 # module "db" {
 #   source  = "terraform-aws-modules/rds/aws"
--- infra/kms.tf
+++ infra/kms.tf
@@ -1,9 +1,9 @@
-resource "aws_kms_key" "default" {
+resource "aws_kms_key" "ssm" {
   enable_key_rotation     = false
   description             = "KMS Key for Redwood Tutorial SSM"
 }
 
-resource "aws_kms_alias" "default" {
+resource "aws_kms_alias" "ssm" {
   name          = "alias/redwood-tutorial-ssm"
-  target_key_id = join("", aws_kms_key.default.*.id)
+  target_key_id = join("", aws_kms_key.ssm.*.id)
 }
