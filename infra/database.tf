485690a6c4e7fd7e0ac6cca788727386fdb132dc
Jacques Blom <39128232+jacquesnf@users.noreply.github.com>
2020-10-18 16:40:37 +0200

    Fix

--- infra/database.tf
+++ infra/database.tf
@@ -41,9 +41,9 @@ resource "aws_security_group" "public_access" {
 
 resource "aws_security_group_rule" "allow_access" {
   type                     = "ingress"
-  from_port                = module.aurora.this_rds_cluster_port
-  to_port                  = module.aurora.this_rds_cluster_port
+  from_port                = module.db.this_rds_cluster_port
+  to_port                  = module.db.this_rds_cluster_port
   protocol                 = "tcp"
   source_security_group_id = aws_security_group.public_access.id
-  security_group_id        = module.aurora.this_security_group_id
+  security_group_id        = module.db.this_security_group_id
 }
