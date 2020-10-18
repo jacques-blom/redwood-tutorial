7c4fc94b98853e62e851ca4731a705128a9ca50f
Jacques Blom <39128232+jacquesnf@users.noreply.github.com>
2020-10-18 18:46:04 +0200

    KMS

--- infra/kms.tf
+++ infra/kms.tf
@@ -1,9 +1,9 @@
-module "kms_key" {
-  source                  = "git::https://github.com/cloudposse/terraform-aws-kms-key.git?ref=0.7.0"
-  namespace               = "redwood-tutorial"
-  stage                   = "prod"
-  name                    = "main"
-  description             = "Main KMS key for the Redwood Tutorial"
+resource "aws_kms_key" "default" {
   enable_key_rotation     = false
-  alias                   = "alias/redwood-tutorial"
+  description             = "KMS Key for Redwood Tutorial SSM"
+}
+
+resource "aws_kms_alias" "default" {
+  name          = "alias/redwood-tutorial-ssm"
+  target_key_id = join("", aws_kms_key.default.*.id)
 }
